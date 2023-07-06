624fd69ebe0c3b29a35f6ff2cd4e1fdba103e39a
Andrey <andrey6458@yandex.ru>
2023-07-06 16:28:55 +0500

    add tests

--- tests.py
+++ tests.py
@@ -1,15 +1,13 @@
 import unittest
-import main
+import Befunge
+
 
 class MyTestCase(unittest.TestCase):
     def test_Fibonachi(self):
-        self.assertEqual(main.Interpreter('Fibonachi.txt').start(), '0 1 1 2 3 5 8 13 21 34 55 89 144 233 ')
+        self.assertEqual(Befunge.Interpreter('programs/Fibonachi.txt').start(), '0 1 1 2 3 5 8 13 21 34 55 89 144 233 ')
 
     def test_HelloWorld(self):
-        self.assertEqual(main.Interpreter('Hello_world.txt').start(), 'Hello World!')
-
-    def test_input_and_print(self):
-        self.assertEqual(main.Interpreter('input.txt').start(), 'Hello World!')
+        self.assertEqual(Befunge.Interpreter('programs/Hello_world.txt').start(), 'Hello World!')
 
 
 if __name__ == '__main__':
